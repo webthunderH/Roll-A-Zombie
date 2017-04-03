f242f0d902537cc89e407d0c938b3fc0f435ae0f
ThunderH <henry.dedonder@gmail.com>
2017-04-03 13:47:00 +0200

    Roll a Zombie Project

---
+++ Assets/GameManger.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManger : MonoBehaviour {
+
+	private int selectedZombiePosition = 0;
+	public Text scoreText;
+	private int score = 0;
+	public GameObject selectedZombie;
+	public List<GameObject> zombies;
+	public Vector3 selectedSize;
+	public Vector3 defaultSize;
+	// Use this for initialization
+	void Start () {
+		SelectZombie(selectedZombie);
+		scoreText.text = "Score : " + score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown("left"))
+		{
+			GetZombieLeft();
+		}
+		if (Input.GetKeyDown("right"))
+		{
+			GetZombieRight();
+		}
+		if (Input.GetKeyDown("up"))
+		{
+			PushUp();
+		}	
+	}
+
+	void GetZombieLeft()
+	{
+		if (selectedZombiePosition == 0)
+		{
+			selectedZombiePosition = 3;
+			SelectZombie(zombies[selectedZombiePosition]);
+		}
+		else
+		{
+			selectedZombiePosition = selectedZombiePosition - 1;
+			SelectZombie(zombies[selectedZombiePosition]);
+		}	
+	}
+
+	void GetZombieRight()
+	{
+		if (selectedZombiePosition == 3)
+		{
+			selectedZombiePosition = 0;
+			SelectZombie(zombies[selectedZombiePosition]);
+		}
+		else
+		{
+			selectedZombiePosition = selectedZombiePosition + 1;
+			SelectZombie(zombies[selectedZombiePosition]);
+		}
+	}
+
+	void SelectZombie(GameObject newZombie)
+	{
+		selectedZombie.transform.localScale = defaultSize;
+		selectedZombie = newZombie;
+		selectedZombie.transform.localScale=selectedSize;
+	}
+
+	void PushUp()
+	{
+		Rigidbody rigid = selectedZombie.GetComponent<Rigidbody>();
+		rigid.AddForce(0, 0, 10, ForceMode.Impulse);
+	}
+
+	public void AddPoint()
+	{
+		score++;
+		scoreText.text = "Score : " + score;
+	}
+}
---
+++ Assets/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+	public GameManger Gm;
+	public AudioClip hit;
+	AudioSource source;
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Gm.AddPoint();
+		source.PlayOneShot(hit);
+	}
+}
